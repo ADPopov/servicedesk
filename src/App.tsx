451735f6b26a2132d0f66af1126d52878b9cea03
ad.popov.it@gmail.com <562023027Ad>
2021-10-18 06:53:45 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ const App: FC = () => {
     return (
         <MainContainer>
             <GridLayout>
-                <Logotype/>
+                <div></div>
                 <Header/>
                 <NavMenu/>
                 <Content/>
