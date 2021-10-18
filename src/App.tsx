3ccec2311f6ebee8904f9d2045570403e32bf7e5
ad.popov.it@gmail.com <562023027Ad>
2021-10-18 07:24:39 -0700

    new logo

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ const App: FC = () => {
     return (
         <MainContainer>
             <GridLayout>
-                <div></div>
+                <Logotype/>
                 <Header/>
                 <NavMenu/>
                 <Content/>
